89b19db46685c388f25df2976b7d3698096721c3
Stefan Cline <77254039+StefanCline@users.noreply.github.com>
2021-12-28 15:21:53 -0800

    Python Sift Complete

--- Python/sandbox.py
+++
@@ -1,9 +0,0 @@
-class Dog():
-    
-    def __init__(self,breed):
-        self.breed = breed
-
-my_dog = Dog(breed = 'Lab')
-
-print(type(my_dog))
-print(my_dog.breed)
--- Python/sandbox2.py
+++
@@ -1,9 +0,0 @@
-print(2 % 10)
-
-mylist = [1]*10
-print(mylist)
-
-print(sum(mylist))
-
-n = input("enter integer: ")
-print(n)
